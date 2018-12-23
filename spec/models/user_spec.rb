d38e491c526c7d6dbbbca8d757c713ba67d7c238
Felipe Menelau <naffowna@yahoo.com.br>
2018-12-23 12:15:33 -0300

    Adiciona testes de usuário

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    user = User.new(name: 'Carlos', email: 'carlos@email.com', password: '1234')
+    expect(user).to be_valid
+  end
+
+  it 'isnt valid without an email' do
+    user = User.new(name: 'Carlos', email: nil, password: '1234')
+    expect(user).to_not be_valid
+  end
+
+  it 'isnt valid without a name' do
+    user = User.new(name: nil, email: 'carlos@email.com', password: '1234')
+    expect(user).to_not be_valid
+  end
 end
