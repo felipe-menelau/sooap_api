20d620fa69af33314c6ed39ad097a3df4cf64399
Felipe Menelau <naffowna@yahoo.com.br>
2018-12-23 17:47:01 -0300

    Move current_user para seu proprio metodo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_request
-  attr_reader :current_user
 
   private
 
   def authenticate_request
-    @current_user = AuthorizeApiRequest.call(request.headers).result
-    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+    render json: { error: 'Not Authorized' }, status: 401 unless current_user
+  end
+
+  def current_user
+    @current_user |= AuthorizeApiRequest.call(request.headers).result
   end
 end
