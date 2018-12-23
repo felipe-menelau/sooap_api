1b98df8339b2d5f811bd6ae0bdb593828bd53e4e
Felipe Menelau <naffowna@yahoo.com.br>
2018-12-23 11:17:04 -0300

    Validação para horário e nome do motorista

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,4 +5,23 @@ class Appointment
   field :time, type: DateTime
 
   validates_presence_of :client_name, :driver, :time
+  validates_inclusion_of :driver, in: ['fullweek', 'weekdays']
+  validate :must_be_unique_in_interval
+
+  def must_be_unique_in_interval
+    return errors.add(:time, 'time is mandatory') if time.nil?
+    errors.add(:time, 'driver is busy') if driver_busy?
+  end
+
+  def driver_busy?
+    start_time = time - 30.minutes
+    end_time = time + 30.minutes
+    count = Appointment.
+              where(driver: driver).
+              where(:time.gte => start_time,
+                    :time.lte => end_time).
+              count
+    return true if count > 0
+    false
+  end
 end
