d31bfbad40d5f9eb0392c412357c046435fe5ab3
Felipe Menelau <naffowna@yahoo.com.br>
2018-12-23 12:41:01 -0300

    Adiciona teste para login com credenciais incorretas

--- spec/controllers/login_controller_spec.rb
+++ spec/controllers/login_controller_spec.rb
@@ -10,6 +10,14 @@ RSpec.describe LoginController, type: :controller do
       params = {:email => 'example@mail.com', :password => '123123123'}
       post(:login, :params => params)
       expect(response.status).to eq(200)
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response['auth_token']).to be
+    end
+
+    it 'shouldnt login with invalid params' do
+      params = {:email => 'example@mail.com', :password => 'errado'}
+      post(:login, :params => params)
+      expect(response.status).to eq(401)
     end
   end
 end
