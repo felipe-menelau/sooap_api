275b687a6df05784ce71fe27ea7b3fe70f265f1f
Felipe Menelau <naffowna@yahoo.com.br>
2018-12-23 19:36:14 -0300

    Adiciona testes para outros casos durante criação de appointment

--- spec/controllers/appointments_controller_spec.rb
+++ spec/controllers/appointments_controller_spec.rb
@@ -13,5 +13,27 @@ RSpec.describe AppointmentsController, type: :controller do
       expect(response.status).to eq(201)
       expect(Appointment.count).to eq(1)
     end
+
+    it 'shouldnt create appointment when there are no drivers' do
+      Appointment.create(client_name: 'Carlos', client_id: @user.id, driver: 'fullweek', time: '2018-12-23T19:20')
+      params = { time: '2018-12-23T19:25' }
+      post(:create, :params => params)
+      expect(response.status).to eq(422)
+      expect(Appointment.count).to eq(1)
+    end
+
+    it 'shouldnt create appointment with no time' do
+      post(:create)
+      expect(response.status).to eq(422)
+      expect(Appointment.count).to eq(0)
+    end
+
+    it 'should create 2 appointments in similar times if on weekday' do
+      Appointment.create(client_name: 'Carlos', client_id: @user.id, driver: 'fullweek', time: '2018-12-25T19:20')
+      params = { time: '2018-12-25T19:25' }
+      post(:create, :params => params)
+      expect(response.status).to eq(201)
+      expect(Appointment.count).to eq(2)
+    end
   end
 end
