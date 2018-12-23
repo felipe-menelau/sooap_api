dfb4a0070db9e5360888cdac7175386d9da50970
Felipe Menelau <naffowna@yahoo.com.br>
2018-12-23 11:08:05 -0300

    Limpar mongodb após cada teste

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -50,4 +50,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.before(:each) do
+    Mongoid.default_client.collections.select {|c| c.name !~ /system/}.each(&:drop)
+  end
 end
