9ed683559879e3593535b5f0aa01e9016d29573f
Felipe Menelau <naffowna@yahoo.com.br>
2018-12-23 20:03:45 -0300

    Ajusta before do teste

--- spec/controllers/appointments_controller_spec.rb
+++ spec/controllers/appointments_controller_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe AppointmentsController, type: :controller do
+  before do
+    @user = User.new(name: 'Carlos', email: 'carlos@email.com', password: '1234')
+    allow(controller).to receive(:authenticate_request).and_return(true)
+    allow(controller).to receive(:current_user).and_return(@user)
+  end
+
   describe 'POST create' do
-    before do
-      @user = User.new(name: 'Carlos', email: 'carlos@email.com', password: '1234')
-      allow(controller).to receive(:authenticate_request).and_return(true)
-      allow(controller).to receive(:current_user).and_return(@user)
-    end
     it 'should create an appointment with valid params' do
       params = { time: '2018-12-25T19:25' }
       post(:create, :params => params)
