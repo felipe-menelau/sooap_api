69a7ce54a6d72900a122d81113ee985fe19c1a04
Felipe Menelau <naffowna@yahoo.com.br>
2018-12-23 19:24:33 -0300

    Implementação da criação de appointment

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,41 @@
+class AppointmentsController < ApplicationController
+  def create
+    time = DateTime.parse(time_params[:time])
+    appointment = create_appointment(time)
+
+    if appointment.save
+      render json: { appointment: serialize_appointment(appointment) }, status: :created
+    else
+      render json: { error: 'no drivers available at that time' }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def time_params
+    params.permit(:time)
+  end
+
+  def create_appointment(time)
+    Appointment.new(time: time,
+                    client_id: current_user.id,
+                    client_name: current_user.name,
+                    driver: decide_driver(time))
+  end
+
+  def decide_driver(time)
+    Appointment::POSSIBLE_DRIVERS.each do |driver|
+      if Appointment.driver_working?(driver, time) && !Appointment.driver_busy?(driver, time)
+        return driver
+      end
+    end
+    nil
+  end
+
+  def serialize_appointment(appointment)
+    {
+      client_name: appointment.client_name,
+      time: appointment.time
+    }
+  end
+end
