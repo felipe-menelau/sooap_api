7dcfb99158886e2372c4b852178e5318643ab8e6
Felipe Menelau <naffowna@yahoo.com.br>
2018-12-23 10:31:20 -0300

    Valida presença de campos obrigatórios em appointment

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe Appointment, :type => :model do
     appointment = Appointment.new(client_name: 'Carlos', driver: 'fullweek', time: DateTime.now)
     expect(appointment).to be_valid
   end
+
+  it 'isnt valid without client_name' do
+    appointment = Appointment.new(client_name: nil, driver: 'fullweek', time: DateTime.now)
+    expect(appointment).to_not be_valid
+  end
+
+  it 'isnt valid without driver' do
+    appointment = Appointment.new(client_name: 'Carlos', driver: nil, time: DateTime.now)
+    expect(appointment).to_not be_valid
+  end
+
+  it 'isnt valid without time' do
+    appointment = Appointment.new(client_name: 'Carlos', driver: 'fullweek', time: nil)
+    expect(appointment).to_not be_valid
+  end
 end
