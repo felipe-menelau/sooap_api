7104c5ae7187b20d968db4c0c38e457402ea5760
Felipe Menelau <naffowna@yahoo.com.br>
2018-12-23 17:23:26 -0300

    Adiciona expectation de user count

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,12 +6,14 @@ RSpec.describe UsersController, type: :controller do
       params = {:email => 'example@mail.com', name: 'Carlos', :password => '123123123'}
       post(:create, :params => params)
       expect(response.status).to eq(201)
+      expect(User.count).to eq(1)
     end
 
     it 'shouldnt create user when params are invalid' do
       params = { name: 'Carlos', :password => '123123123'}
       post(:create, :params => params)
       expect(response.status).to eq(422)
+      expect(User.count).to eq(0)
     end
   end
 end
