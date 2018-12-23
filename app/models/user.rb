b6d3232dfdf4b353581719d0a0f3a07834ca608a
Felipe Menelau <naffowna@yahoo.com.br>
2018-12-23 12:09:45 -0300

    Modifica bcrypt para jeito compativel com mongoid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User
   include Mongoid::Document
-  has_secure_password
+  include BCrypt
+
   field :name, type: String
   field :email, type: String
   field :password_digest, type: String
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
+  end
 end
