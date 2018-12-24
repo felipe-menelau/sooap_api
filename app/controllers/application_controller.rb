eddcfb1b346faa3fe2e9119f29f262311815f287
Felipe Menelau <naffowna@yahoo.com.br>
2018-12-24 09:07:33 -0300

    Corrige comportamento do current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_request
+  attr_reader :current_user
 
   private
 
   def authenticate_request
-    render json: { error: 'Not Authorized' }, status: 401 unless current_user
+    render json: { error: 'Not Authorized' }, status: 401 unless set_current_user
   end
 
-  def current_user
-    @current_user |= AuthorizeApiRequest.call(request.headers).result
+  def set_current_user
+    @current_user = AuthorizeApiRequest.call(request.headers).result
   end
 end
